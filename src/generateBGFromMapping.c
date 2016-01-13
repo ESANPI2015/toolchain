3d8ea558d1f59b1ad413399bd83f88a0a3a15a9a
Moritz Schilling <moritz.schilling@dfki.de>
2016-01-13 13:19:49 +0100

    Changed toolchain
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- src/generateBGFromMapping.c
+++ src/generateBGFromMapping.c
@@ -1,48 +1,34 @@
 #include "sw_to_hw_map.h"
 #include "sw_to_hw_map_yaml.h"
+#include "node_list.h"
+#include "node_types/bg_node_subgraph.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
-static bg_error generateSubGraph(sw2hw_map_t *map, const hw_node_t *target, const char* prefix)
-{
-    bg_error err;
-    bg_graph_t *graph;
-    char name[bg_MAX_STRING_LENGTH];
-
-    snprintf(name, bg_MAX_STRING_LENGTH, "%s_%u.bg", prefix, target->id);
-    fprintf(stderr, "Generate Behaviour Graph for target %u to file \"%s\"\n", target->id, name);
-    err = bg_graph_alloc(&graph, name);
-    sw2hw_map_create_subgraph(map, target->id, graph);
-    err = bg_graph_to_yaml_file(name, graph);
-    err = bg_graph_free(graph);
-    return err;
-}
-
 int main (int argc, char *argv[])
 {
     int opt;
-    sw2hw_map_t mapping;
-    hw_node_t *target;
-    priority_list_iterator_t it;
+    sw2hw_map_t mapping, transformed;
+    bg_graph_t *sub;
+    bg_node_t *node;
+    bg_node_list_iterator_t it;
     char mapFileName[SW2HW_MAX_STRING_LENGTH];
-    char targetArg[SW2HW_MAX_STRING_LENGTH];
+    char outFileName[SW2HW_MAX_STRING_LENGTH];
     char prefix[bg_MAX_STRING_LENGTH];
-    unsigned int hwId;
-    bool allTargets = true;
+    char fileName[bg_MAX_STRING_LENGTH];
 
-    printf("Map to Behaviour Graph\n");
-    while ((opt = getopt(argc, argv, "ht:")) != -1)
+    printf("Mapping to Behaviour Graph(s)\n");
+    while ((opt = getopt(argc, argv, "hp:")) != -1)
     {
         switch (opt)
         {
-            case 't':
-                snprintf(targetArg, SW2HW_MAX_STRING_LENGTH, "%s", optarg);
-                allTargets = false;
+            case 'p':
+                snprintf(prefix, bg_MAX_STRING_LENGTH, "%s", optarg);
                 break;
             case 'h':
             default:
-                fprintf(stderr, "Usage: %s [-t target] map prefix-of-output\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-p prefix-of-subgraphs] map-file transformed-map-file\n", argv[0]);
                 exit(EXIT_FAILURE);
                 break;
         }
@@ -50,13 +36,16 @@ int main (int argc, char *argv[])
 
     if ((optind >= argc) || (argc < 3))
     {
-        fprintf(stderr, "Usage: %s [-t target] map prefix-of-output\n", argv[0]);
+        fprintf(stderr, "Usage: %s [-p prefix-of-subgraphs] map-file transformed-map-file\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     snprintf(mapFileName, SW2HW_MAX_STRING_LENGTH, "%s", argv[optind]);
-    snprintf(prefix, bg_MAX_STRING_LENGTH, "%s", argv[optind+1]);
+    snprintf(outFileName, SW2HW_MAX_STRING_LENGTH, "%s", argv[optind+1]);
 
+    /*Init*/
     bg_initialize();
+    sw2hw_map_init(&mapping, NULL, NULL);
+    sw2hw_map_init(&transformed, NULL, NULL);
     sw2hw_map_from_yaml_file(mapFileName, &mapping);
     if (!priority_list_size(mapping.assignments))
     {
@@ -64,32 +53,24 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if (allTargets)
+    /*Transformation*/
+    sw2hw_map_transform(&transformed, &mapping);
+    /*Storing*/
+    for (node = bg_node_list_first(transformed.swGraph->hidden_nodes, &it);
+            node;
+            node = bg_node_list_next(&it))
     {
-        for (target = priority_list_first(mapping.hwGraph->nodes, &it);
-                target;
-                target = priority_list_next(&it))
-            generateSubGraph(&mapping, target, prefix);
-    } else {
-        if (!sscanf(targetArg, "%u", &hwId))
-        {
-            for (target = priority_list_first(mapping.hwGraph->nodes, &it);
-                    target;
-                    target = priority_list_next(&it))
-                if (strcmp(target->name, targetArg) == 0)
-                    hwId = target->id;
-        }
-        target = hw_graph_get_node(mapping.hwGraph, hwId);
-        if (!target)
-        {
-            fprintf(stderr, "Could not find target\n");
-            exit(EXIT_FAILURE);
-        }
-        printf("Using target id %u with name %s\n", target->id, target->name);
-        generateSubGraph(&mapping, target, prefix);
+        sub = ((subgraph_data_t *)node->_priv_data)->subgraph;
+        /*Ensure that file and node name are the same ... otherwise we loose correspondence*/
+        snprintf(fileName, bg_MAX_STRING_LENGTH, "%s_%s.bg", prefix, node->name);
+        snprintf((char *)sub->name, bg_MAX_STRING_LENGTH, "%s", fileName);
+        bg_graph_to_yaml_file(fileName, ((subgraph_data_t *)node->_priv_data)->subgraph);
     }
+    sw2hw_map_to_yaml_file(outFileName, &transformed);
 
+    /*Deinit*/
     sw2hw_map_destroy(&mapping);
+    sw2hw_map_destroy(&transformed);
     bg_terminate();
     exit(EXIT_SUCCESS);
 }
--- src/mapper.c
+++ src/mapper.c
@@ -1,6 +1,6 @@
 #include "sw_to_hw_map.h"
 #include "sw_to_hw_map_yaml.h"
-#include "priority_list.h"
+#include "hwg_yaml.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -41,16 +41,22 @@ int main (int argc, char *argv[])
     snprintf(swYamlFile, bg_MAX_STRING_LENGTH, "%s", argv[optind+1]);
     snprintf(outfileName, SW2HW_MAX_STRING_LENGTH, "%s", argv[optind+2]);
 
+    /*Initialization*/
     bg_initialize();
 
-    sw2hw_map_init(&mapping, hwYamlFile, swYamlFile);
+    sw2hw_map_init(&mapping, NULL, NULL);
+    bg_graph_from_yaml_file(swYamlFile, mapping.swGraph);
+    hw_graph_from_yaml_file(hwYamlFile, mapping.hwGraph);
+    /*Mapping*/
     sw2hw_map_match(&mapping);
     sw2hw_map_initial(&mapping, defaultCost);
     do {
         while (sw2hw_map_refine(&mapping, defaultCost) == SW2HW_ERR_NONE);
     } while (sw2hw_map_regroup(&mapping, defaultCost) == SW2HW_ERR_NONE);
+    /*Storing*/
     sw2hw_map_to_yaml_file(outfileName, &mapping);
 
+    /*Deinit*/
     sw2hw_map_destroy(&mapping);
     bg_terminate();
     exit(EXIT_SUCCESS);
